2d006d0e62984b4001349ec051f7a2eda5d2dfb8
Dobrosław Żybort <matrixik@gmail.com>
2017-12-29 15:07:48 +0100

    Remove sending emails and save working sites to file

--- README.md
+++ README.md
@@ -1,23 +1,12 @@
 isOnline
 ========
 
-Script for scheduled check of websites availability and mail report. Suited for crontab job.
+Script for scheduled check of websites availability.
 
 ## Config
-1. Create ```emails.lst``` file and fill it with each email in new line (as in ```emails.lst.example```). leave an empty line in the end.
-2. Create ```websites.lst``` file and fill it with each website in a new line (as in ```websites.lst.example```). leave an empty line in the end.
+1. Create ```websites.lst``` file and fill it with each website in a new line (as in ```websites.lst.example```). leave an empty line in the end.
 
-    Note that the script will follow url(s) with 301 (redirect).
-
-## Add to crontab
-Add the following lines to crontab config ($ crontab -e) 
-
-```
-THIS_IS_CRON=1
-*/30 * * * * /path/to/isOnline/checker.sh
-```
-
-in this example crontab will run ```checker.sh``` every 30min.
+    Note that the script will follow url(s) with 30x (redirect).
 
 by [ET][ET].
 
--- checker.sh
+++ checker.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 # Website status checker. by ET (etcs.me)
 
-WORKSPACE=/scripts/isOnline
-# list of websites. each website in new line. leave an empty line in the end.
+# Source: https://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+WORKSPACE="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+# List of websites. each website in new line. leave an empty line in the end.
 LISTFILE=$WORKSPACE/websites.lst
-# Send mail in case of failure to. leave an empty line in the end.
-EMAILLISTFILE=$WORKSPACE/emails.lst
 # Temporary dir
 TEMPDIR=$WORKSPACE/cache
+# Append working URIs to this file
+WORKINGFILE=$WORKSPACE/working.lst
 
 # `Quiet` is true when in crontab; show output when it's run manually from shell.
 # Set THIS_IS_CRON=1 in the beginning of your crontab -e.
@@ -15,30 +23,29 @@ TEMPDIR=$WORKSPACE/cache
 if [ -n "$THIS_IS_CRON" ]; then QUIET=true; else QUIET=false; fi
 
 function test {
-  response=$(curl -L --write-out %{http_code} --silent --output /dev/null $1)
+  response=$(curl -L --write-out "%{http_code}" --silent --output /dev/null $1)
   filename=$( echo $1 | cut -f1 -d"/" )
   if [ "$QUIET" = false ] ; then echo -n "$p "; fi
 
-  if [ $response -eq 200 ] ; then
-    # website working
-    if [ "$QUIET" = false ] ; then
-      echo -n "$response "; echo -e "\e[32m[ok]\e[0m"
-    fi
-    # remove .temp file if exist 
-    if [ -f $TEMPDIR/$filename ]; then rm -f $TEMPDIR/$filename; fi
-  else
-    # website down
-    if [ "$QUIET" = false ] ; then echo -n "$response "; echo -e "\e[31m[DOWN]\e[0m"; fi
-    if [ ! -f $TEMPDIR/$filename ]; then
-        while read e; do
-            # using mailx command
-            echo "$p WEBSITE DOWN" | mailx -s "$1 WEBSITE DOWN ( $response )" $e
-            # using mail command
-            #mail -s "$p WEBSITE DOWN" "$EMAIL"
-        done < $EMAILLISTFILE
+  case "$response" in
+    200|300|301|302|304|307|308)
+      # website working
+      if [ "$QUIET" = false ] ; then
+        echo -n "$response "; echo -e "\e[32m[ok]\e[0m"
+        echo "$filename" >> $WORKINGFILE
+      fi
+      # remove .temp file if exist
+      if [ -f $TEMPDIR/$filename ]; then rm -f $TEMPDIR/$filename; fi
+      ;;
+    *)
+      # website down
+      if [ "$QUIET" = false ] ; then echo -n "$response "; echo -e  "\e[31m[DOWN]\e[0m"; fi
+      if [ ! -f $TEMPDIR/$filename ]; then
+        echo "$p WEBSITE DOWN"
         echo > $TEMPDIR/$filename
-    fi
-  fi
+      fi
+      ;;
+  esac
 }
 
 # main loop
